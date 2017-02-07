9b2421b03b57b004b603ce0df486b44ecbb68b3a
JanHuang <bboyjanhuang@gmail.com>
2017-02-07 12:02:34 +0800

    add swoole serialize

---
+++ src/Swoole.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author    jan huang <bboyjanhuang@gmail.com>
+ * @copyright 2016
+ *
+ * @link      https://www.github.com/janhuang
+ * @link      http://www.fast-d.cn/
+ */
+
+namespace FastD\Packet;
+
+
+/**
+ * Class Swoole
+ * @package FastD\Packet
+ */
+class Swoole implements PacketInterface
+{
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public static function encode($data)
+    {
+        return swSerialize::pack($data);
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public static function decode($data)
+    {
+        return swSerialize::unpack($data);
+    }
+}
